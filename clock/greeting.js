ba9f06aa0e76e850d49594acaf3e31cdcd07f3f1
swc0620 <59383615+swc0620@users.noreply.github.com>
2020-04-21 23:41:19 +0900

    Today I coded Clock App using JavaScript

---
+++ clock/greeting.js
@@ -0,0 +1,45 @@
+const form = document.querySelector(".js-form");
+const input = form.querySelector("input");
+const greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser";
+const SHOWING_ON = "showing";
+
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(USER_LS);
+    form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text) {
+    form.classList.remove(SHOWING_ON);
+    greeting.classList.add(SHOWING_ON);
+    greeting.innerText = `Hello ${text}`;
+}
+
+function loadName() {
+    const currentUser = localStorage.getItem(USER_LS);
+    if(currentUser === null){
+        // she is not
+        askForName();
+    } else {
+        // she is
+        paintGreeting(currentUser);
+    }
+}
+
+function init(){
+
+}
+
+init();
